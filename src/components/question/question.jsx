2217e6fbb7cfbd4862eaddb8c3f831d2203ea458
Rob <robrichardsdpt@gmail.com>
2021-02-27 16:50:34 -0500

    logic is giving the right answer.  Next will be to add stats and improve the UI.

--- src/components/question/question.jsx
+++ src/components/question/question.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './question.scss'
 
 const Question = ({ randomCountry, options, countries }) => {
+  const [status, setStatus] = useState('not answered')
+  const [countryResponse, setCountryResponse] = useState('')
   console.log(randomCountry)
+  
+  const handleSubmit = (event) => {
+    console.log(event.target.id)
+    if(countries[event.target.id].name === randomCountry.name) setStatus('correct')
+    if(countries[event.target.id].name !==randomCountry.name) setStatus('wrong')
+    setCountryResponse(countries[event.target.id].name)
+  }
+
   const optionsRandomized = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -13,14 +23,18 @@ const Question = ({ randomCountry, options, countries }) => {
   const optionsRandomizedArray = optionsRandomized(options)
   const optionsJsx = optionsRandomizedArray.map((country) => {
     return(
-      <div key={country}>{countries[country].name}</div>)
+      <div className='option' key={country} id={country} onClick={handleSubmit}>{countries[country].name}</div>)
  })
   console.log(optionsRandomizedArray)
   return(
     <div className='question-card'>
     Which Country does this flag belong to?
       <div><img src={randomCountry.flag} className='flag-image'/></div>
-      {optionsJsx}
+      {status === 'not answered' && optionsJsx}
+      { (status === 'not answered') && 'What do you think?' }
+      { status === 'correct' &&  `You are right!  ${countryResponse} is the correct answer` }
+      { status === 'wrong' && `You are wrong! ${countryResponse} is the wrong answer` }
+
     </div>
   )
 }
