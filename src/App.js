a6c26c4c9d755f6f51eebb343e7340b5710cd9e7
Rob <robrichardsdpt@gmail.com>
2021-02-27 14:51:30 -0500

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './components/home/home.jsx'
 import Quiz from './components/quiz/quiz.jsx'
+import Header from './components/header/header.jsx'
 import { Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+    <Header/>
     We are clean and good to go!
     <Route exact path='/'component={Home} />
     <Route path='/quiz' component={Quiz} />
